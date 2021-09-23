1696898f1b1ec5f5429be18914927681eb02fa43
nefertirii <nefertiri972@gmail.com>
2021-09-23 18:22:23 +0900

    Add project

---
+++ src/test/java/io/security/core/CoreSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
