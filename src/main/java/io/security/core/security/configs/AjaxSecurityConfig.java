729c0e89214e15f2eb992f4d5ec818299d82c391
nefertirii <nefertiri972@gmail.com>
2021-09-28 02:40:49 +0900

    Add ajax authentication success, failure handler
    - Ajax 인증 성공, 실패 핸들러 추가
    - 성공, 실패 시 각각 상황에 맞는 JSON 데이터 반환

--- src/main/java/io/security/core/security/configs/AjaxSecurityConfig.java
+++ src/main/java/io/security/core/security/configs/AjaxSecurityConfig.java
@@ -1,6 +1,8 @@
 package io.security.core.security.configs;
 
 import io.security.core.security.filter.AjaxLoginProcessingFilter;
+import io.security.core.security.handler.AjaxAuthenticationFailureHandler;
+import io.security.core.security.handler.AjaxAuthenticationSuccessHandler;
 import io.security.core.security.provider.AjaxAuthenticationProvider;
 import io.security.core.security.provider.FormAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
@@ -45,12 +49,23 @@ public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
     public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
         AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
         ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean());
-
+        ajaxLoginProcessingFilter.setAuthenticationSuccessHandler(ajaxAuthenticationSuccessHandler());
+        ajaxLoginProcessingFilter.setAuthenticationFailureHandler(ajaxAuthenticationFailureHandler());
         return ajaxLoginProcessingFilter;
     }
 
     @Bean
     public AuthenticationProvider ajaxAuthenticationProvider() {
         return new AjaxAuthenticationProvider(userDetailsService, passwordEncoder);
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler ajaxAuthenticationSuccessHandler() {
+        return new AjaxAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler ajaxAuthenticationFailureHandler() {
+        return new AjaxAuthenticationFailureHandler();
     }
 }
