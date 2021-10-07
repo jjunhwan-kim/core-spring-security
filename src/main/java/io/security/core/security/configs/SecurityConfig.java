2292033119bf04d70518de3a99d23344c1c8dfae
nefertirii <nefertiri972@gmail.com>
2021-10-08 01:22:33 +0900

    Add filter invocation security metadata source
    - SecurityMetadataSource 객체는 자원에 대한 권한 정보를 내부에 맵으로 관리하고 제공함
      - URL 권한 정보를 추출하기 위해 FilterInvocationSecurityMetadataSource 인터페이스를 구현한 UrlFilterInvocationSecurityMetadataSource 클래스 생성
      - DB에서 URL 권한 정보를 추출하기 위한 객체
    - FilterSecurityInterceptor 필터는 요청에 대한 인가 처리를 하는데, 이 때 요청에 대한 권한 정보를 SecurityMetadataSource 객체로 부터 얻어옴
    - 인증 객체, 권한 정보,  FilterInvocation 객체를 가지고 AccessDecisionManager 에게 인가 처리를 위임함

--- src/main/java/io/security/core/security/configs/SecurityConfig.java
+++ src/main/java/io/security/core/security/configs/SecurityConfig.java
@@ -4,12 +4,17 @@ import io.security.core.security.common.FormWebAuthenticationDetailsSource;
 import io.security.core.security.handler.FormAccessDeniedHandler;
 import io.security.core.security.handler.FormAuthenticationFailureHandler;
 import io.security.core.security.handler.FormAuthenticationSuccessHandler;
+import io.security.core.security.metadatasource.UrlFilterInvocationSecurityMetadataSource;
 import io.security.core.security.provider.FormAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -21,11 +26,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
 
 @Order(1)
 @RequiredArgsConstructor
@@ -52,9 +61,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "/users", "/login*").permitAll()
                 //.antMatchers("/css/**", "/images/**", "/js/**").permitAll()
-                .antMatchers("/mypage").hasRole("USER")
-                .antMatchers("/messages").hasRole("MANAGER")
-                .antMatchers("/config").hasRole("ADMIN")
+                //.antMatchers("/mypage").hasRole("USER")
+                //.antMatchers("/messages").hasRole("MANAGER")
+                //.antMatchers("/config").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -67,7 +76,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .exceptionHandling()
-                .accessDeniedHandler(formAccessDeniedHandler());
+                .accessDeniedHandler(formAccessDeniedHandler())
+                .and()
+                .addFilterBefore(customFilterSecurityInterceptor(), FilterSecurityInterceptor.class);
     }
 
     @Bean
@@ -98,5 +109,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AccessDeniedHandler formAccessDeniedHandler() {
         return new FormAccessDeniedHandler("/denied");
+    }
+
+    @Bean
+    public FilterSecurityInterceptor customFilterSecurityInterceptor() throws Exception {
+
+        FilterSecurityInterceptor filterSecurityInterceptor = new FilterSecurityInterceptor();
+        filterSecurityInterceptor.setSecurityMetadataSource(urlFilterInvocationSecurityMetadataSource());
+        filterSecurityInterceptor.setAccessDecisionManager(affirmativeBased());
+        filterSecurityInterceptor.setAuthenticationManager(authenticationManagerBean());
+        return filterSecurityInterceptor;
+    }
+
+    @Bean
+    public FilterInvocationSecurityMetadataSource urlFilterInvocationSecurityMetadataSource() {
+        return new UrlFilterInvocationSecurityMetadataSource();
+    }
+
+    private AccessDecisionManager affirmativeBased() {
+        return new AffirmativeBased(getAccessDecisionVoters());
+    }
+
+    private List<AccessDecisionVoter<?>> getAccessDecisionVoters() {
+        return Arrays.asList(new RoleVoter());
     }
 }
